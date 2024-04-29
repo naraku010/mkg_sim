5df31b9e128f43bdcff6c43a151793a0e3219aa8
jinwoo <naraku010@gmail.com>
2024-04-29 16:01:09 +0900

    hbhn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ColorUtil from "./util/color";
+import Home from "./pages/Home";
+import "./App.scss";
 
-function App() {
+import * as colorwaysActions from "./store/slices/colorways";
+import * as settingsActions from "./store/slices/settings";
+
+export default function App() {
+  const colorway_id = useSelector(colorwaysActions.selectColorway);
+  const sceneAutoColor = useSelector(settingsActions.selectSceneAutoColor);
+  const sceneColor = useSelector(settingsActions.selectSceneColor);
+  const highContrast = useSelector(settingsActions.selectHighContrast);
+
+  const getAccent = () => {
+    return ColorUtil.getUiAccent(colorway_id);
+  };
+
+  const getSceneColor = () => {
+    return sceneAutoColor ? ColorUtil.getAccent(colorway_id) : sceneColor;
+  };
+
+  const uiColors = {
+    "--main": "#202024",
+    "--dark-1": "#202024",
+    "--dark-2": "#202024",
+    "--light-1": "#38383f",
+    "--light-2": "#9c9ca7",
+    "--light-3": "#e0e0e3",
+    "--accent": getAccent(),
+    "--sceneColor": getSceneColor(),
+    "--accent-transparent": ColorUtil.getTransparentColor(getAccent()),
+    "--compliment": ColorUtil.getUiCompliment(colorway_id),
+    "--accent-text": ColorUtil.getUiAccentText(colorway_id),
+  };
+
+  const uiColorsHC = {
+    "--main": "#000000",
+    "--dark-1": "#202024",
+    "--dark-2": "#202024",
+    "--light-1": "#202024",
+    "--light-2": "#cccccc",
+    "--light-3": "#ffffff",
+    "--accent": getAccent(),
+    "--sceneColor": getSceneColor(),
+    "--accent-transparent": ColorUtil.getTransparentColor(getAccent()),
+    "--compliment": ColorUtil.getUiCompliment(colorway_id),
+    "--accent-text": "#000000",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" style={highContrast ? uiColorsHC : uiColors}>
+        <Router>
+          <Home />
+          <Switch>
+            <Route path="/"></Route>
+          </Switch>
+        </Router>
+      </div>
   );
 }
-
-export default App;
