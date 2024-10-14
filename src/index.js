487009e59efce416a57cd69170024c8d488e40e0
비글이 <naraku010@gmail.com>
2024-10-14 12:23:12 +0900

    모바일 반영

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import store from "./store/store";
 import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
-import firebase from "./firebase"
+// import firebase from "./firebase"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
