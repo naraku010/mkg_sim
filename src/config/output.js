8a7ed282a12093c4ba258e8e1662d3c6830ec92b
egibiz <naraku010@gmail.com>
2025-09-01 16:49:53 +0900

    chore: update .gitignore to exclude .venv directory

--- src/config/output.js
+++ src/config/output.js
@@ -1,17 +1,9 @@
-import gmk_black_gold from "./new_colorways/gmk_black_gold.json";
-import keycobo_soju_blue from "./new_colorways/keycobo_soju_blue.json";
-import gmk_orenji from "./new_colorways/gmk_orenji.json";
-import gmk_just_beachy from "./new_colorways/gmk_just_beachy.json";
-import gmk_just_beachy_alpha from "./new_colorways/gmk_just_beachy_alpha.json";
-import gmk_red_devils from "./new_colorways/gmk_red_devils.json";
-import gmk_city_pop_neue from "./new_colorways/gmk_city_pop_neue.json";
+import gmk_space_cadet from "./organized/GMK/gmk_space_cadet.json";
+import gmk_space_cadet_blue_base from "./organized/GMK/gmk_space_cadet_blue_base.json";
+import gmk_serika from "./organized/GMK/gmk_serika.json";
 
 export const COLORWAYS_NEW = {
-    gmk_black_gold: gmk_black_gold,
-    keycobo_soju_blue: keycobo_soju_blue,
-    gmk_orenji: gmk_orenji,
-    gmk_just_beachy: gmk_just_beachy,
-    gmk_just_beachy_alpha: gmk_just_beachy_alpha,
-    gmk_red_devils: gmk_red_devils,
-    gmk_city_pop_neue: gmk_city_pop_neue
+    gmk_space_cadet: gmk_space_cadet,
+    gmk_space_cadet_blue_base: gmk_space_cadet_blue_base,
+    gmk_serika: gmk_serika
 };
